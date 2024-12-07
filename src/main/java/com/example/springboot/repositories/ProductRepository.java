541a58bb399edaeeaab6f0acdb934ebd9e0b2dd8
wanderson almeida <wandersonalmeida.eng@gmail.com>
2024-12-07 12:35:46 -0300

    implement productRecord and implement PostMapping produtc

--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -2,8 +2,11 @@ package com.example.springboot.repositories;
 
 import com.example.springboot.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface IProductiveRepository extends JpaRepository<ProductModel, UUID> {
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
 }
